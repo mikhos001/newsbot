532b9ff4bae729c2c3d812be92344b26de8535fe
mikh18rus <43314361+mikh18rus@users.noreply.github.com>
2025-02-28 11:39:13 +0400

    Add random prompt selection for funny news generation

--- deepseek.js
+++ deepseek.js
@@ -1,5 +1,6 @@
 import OpenAI from "openai";
 import {config} from "dotenv";
+import {textPrompt} from "./query.js";
 config();
 
 const openai = new OpenAI({
@@ -7,9 +8,16 @@ const openai = new OpenAI({
         apiKey: `${process.env.DEEPSEEK_API_KEY}`
 });
 
+function getRandomPrompt() {
+  if (textPrompt.length === 0) {
+      throw new Error("Array is empty");
+  }
+  return textPrompt[Math.floor(Math.random() * textPrompt.length)];
+}
+
 export async function getFunnyNews() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "Сгенерируй смешную новость ограничившись 200 символами" }],
+    messages: [{ role: "user", content: getRandomPrompt() }],
     model: "deepseek-chat",
   });
   return completion.choices[0].message.content;
